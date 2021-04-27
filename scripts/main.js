9ddb699176496d8d39833f3347f7e52e64f61218
ismayilisrafilov7 <ismayilhanma7@gmail.com>
2021-04-27 16:20:37 +0400

    functions and switch

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,7 @@
+// var isStudent = false;
 
-var isStudent = false;
-
-var x = 5;
-var y = 2;
+// var x = 5;
+// var y = 2;
 
 // console.log(x + y);
 // console.log(x - y);
@@ -16,28 +15,28 @@ var y = 2;
 //     }
 // }
 
-var students = [
-    {
-        id: "1",
-        name: "Seymur",
-        age: 45
-    },
-    {
-        id: "2",
-        name: "Cemil",
-        age: 35
-    },
-    {
-        id: "3",
-        name: "Senay",
-        age: 25
-    },
-    {
-        id: "4",
-        name: "Emil",
-        age: 25
-    }
-]
+// var students = [
+//     {
+//         id: "1",
+//         name: "Seymur",
+//         age: 45
+//     },
+//     {
+//         id: "2",
+//         name: "Cemil",
+//         age: 35
+//     },
+//     {
+//         id: "3",
+//         name: "Senay",
+//         age: 25
+//     },
+//     {
+//         id: "4",
+//         name: "Emil",
+//         age: 25
+//     }
+// ]
 
 // for (var i = 0; i < 10; i++) {
 //     if(i == 6) {
@@ -59,21 +58,87 @@ var students = [
 // Math.pow(5, 2);
 // 5**2;
 
-var isPersonFound = false;
-
+// var isPersonFound = false;
 
 var result = 0;
+var i = 0;
 
-do {
-    input = prompt("Eded daxil edin: ");
-    // result += parseInt("15.7", 10);
-    // result += parseFloat(input);
-    result += +input;
-    if(input == 0) {
+// do {
+//     input = prompt("Eded daxil edin: ");
+//     // result += parseInt("15.7", 10);
+//     // result += parseFloat(input);
+//     if(input == 0) {
+//         break;
+//     }
+//     if(i % 2 === 0) {
+//         result += (+input);
+//     } else {
+//         result *= input;
+//     }
+//     i++;
+// } while(true)
+
+// alert(result);
+
+// var test = "test";
+
+var category = "books";
+
+// if(category === "electronics") {
+//     console.log("Electronics products")
+// } else if(category === "books") {
+//     console.log("Books products")
+// } else if(category === "clothing") {
+//     console.log("clothing products")
+// } else {
+//     console.log("none category")
+// }
+
+// switch(category) {
+//     case "electronics":
+//         console.log("Electronics products");
+//         break;
+//     case "books":
+//         console.log("Books products");
+//         break;
+//     case "clothing":
+//         console.log("clothing products");
+//         break;
+//     default:
+//         console.log("none category");
+//         break;
+// }
+
+// function operators(x, y) {
+//     console.log(x + y);
+//     console.log(x - y);
+//     console.log(x * y);
+//     console.log(x / y);
+//     console.log(x % y);
+// }
+
+// var x = +prompt("X-i daxil et: ");
+// var y = +prompt("Y-i daxil et: ");
+
+// operators(x, y)
+//  function id(name,surname){
+//      console.log(name+" "+surname)
+//  }
+//  id('Ismayil','Israfilov');
+
+ var color = "blue";
+
+ switch(color) {
+    case "blue":
+        console.log("#0000ff");
         break;
-    }
-} while(true)
-
-alert(result);
-
-var test = "test";
+    case "red":
+        console.log("#ff0000");
+        break;
+    case "green":
+        console.log("#00ff00");
+        break;
+    default:
+        console.log("not color")
+     
+ }
